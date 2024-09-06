4b9662aafaf4258b4744172d8635f0517504c3ee
johnrenko <john.dutamby@gmail.com>
2024-09-06 12:12:17 +0200

    keep only magic link

--- lib/supabaseClient.ts
+++ lib/supabaseClient.ts
@@ -10,23 +10,6 @@ export async function signInWithMagicLink(email: string) {
   return { error }
 }
 
-export async function signUpWithEmail(email: string, password: string) {
-  const { data, error } = await supabase.auth.signUp({ email, password })
-  return { data, error }
-}
-
-export async function signInWithEmail(email: string, password: string) {
-  const { data, error } = await supabase.auth.signInWithPassword({ email, password })
-  return { data, error }
-}
-
-export async function signInWithGoogle() {
-  const { data, error } = await supabase.auth.signInWithOAuth({
-    provider: 'google',
-  })
-  return { data, error }
-}
-
 export async function signOut() {
   const { error } = await supabase.auth.signOut()
   return { error }
